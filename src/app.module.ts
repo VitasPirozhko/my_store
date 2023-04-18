2269862c5b66a8e57808586283ffba6abfe36322
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-04-18 16:07:05 +0300

    фінальна версія авторизації

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
