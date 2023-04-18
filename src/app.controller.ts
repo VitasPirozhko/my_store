6b010073f9b0c6ca61b0f9813382e61df756257e
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-04-18 16:02:37 +0300

    +

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Request, Post } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
-import { Public } from './decorators';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
-  @Public()
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.body);
